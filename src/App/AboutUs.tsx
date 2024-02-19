3fea3687822d5784ccc30238d8eb11a1bdcb0e1a
machigurumi <160128471+machigurumi@users.noreply.github.com>
2024-02-19 17:33:31 +0900

    Update AboutUs.tsx
    泉大津の内容に変更

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -18,17 +18,13 @@ const Content = () => {
           <div className="logo">Geolonia PWAマップ</div>
         </div>
 
-        <p>Geolonia PWAマップは、Google スプレッドシートを更新するだけでオリジナルの地図アプリを作成することができます。</p>
-        <p>プログラムはオープンソースで公開しているため、自由にカスタマイズしてご利用いただけます。</p>
+        <p>泉大津まちぐるみ図書館は、泉大津市立図書館シープラを中心に市全体を図書館にみたて、まちなかでどこでも・いつでも市民が本に親しむことができることを目指しています。このまちぐるみ図書館マップはGeolonia PWAマップを利用して作成されました。</p>
+        <p>泉大津まちぐるみ図書館マップに掲載をご希望される場合は、お問い合わせフォームからご連絡ください。</p>
+        
 
-        <h2> Geolonia PWAマップについて</h2>
-        <p><a href="https://blog.geolonia.com/2022/05/17/pwamap-beta.html" target='_blank' rel="noreferrer">https://blog.geolonia.com/2022/05/17/pwamap-beta.html</a></p>
 
-        <p>また、独自ドメインでの利用やデザインのカスタマイズや利用に必要な緯度・経度情報の提供も有償で別途承ります。ご希望の方は下記までお問い合わせください。</p>
-
-        <h2>お問い合わせフォーム</h2>
-        <p><a href="https://geolonia.com/contact/" target='_blank' rel="noreferrer">https://geolonia.com/contact/</a></p>
-        <p>※カスタマイズおよびアプリの作成・利用についてはサポート対象外となります。あらかじめご了承ください。</p>
+        
+        <p><a href="https://forms.gle/5gzyhbpkHWRSTxX97" target='_blank' rel="noreferrer">お問い合わせフォームはこちら</a></p>
 
         {config.form_url?
           <>
